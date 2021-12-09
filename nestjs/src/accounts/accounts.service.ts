2743d288ea70c2baed4c5b6fbfc6eb8137a63661
Jeison de Borba <jeizon.borba@gmail.com>
2021-12-09 02:42:45 -0300

    class three - create nest app

---
+++ nestjs/src/accounts/accounts.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { EmptyResultError } from 'sequelize';
+import { Op } from 'sequelize';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { UpdateAccountDto } from './dto/update-account.dto';
+import { Account } from './entities/account.entity';
+
+@Injectable()
+export class AccountsService {
+
+  constructor(
+    @InjectModel(Account)
+    private accountModel: typeof Account
+  ) {}
+
+  create(createAccountDto: CreateAccountDto) {
+    return this.accountModel.create(createAccountDto);
+  }
+
+  findAll() {
+    return this.accountModel.findAll();
+  }
+
+  async findOne(idOrToken: string) {
+    console.info(`*** entrou em accountService.fineOne com ${idOrToken}`)
+    return this.accountModel.findOne({
+      where: {
+        [Op.or]: {
+          id: idOrToken,
+          token: idOrToken,
+        },
+      },
+      rejectOnEmpty: new EmptyResultError(
+        `Account with ID/Token ${idOrToken} not found`,
+      ),
+    });
+  }
+
+  async update(id: string, updateAccountDto: UpdateAccountDto) {
+    const account = await this.findOne(id);
+    return account.update(updateAccountDto);
+  }
+
+  async remove(id: string) {
+    const account = await this.findOne(id);
+    return account.destroy();
+  }
+}
