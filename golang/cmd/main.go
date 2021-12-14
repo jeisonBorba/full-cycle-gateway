8493792f76e326109716ed9610ec2ec816505a4a
Jeison de Borba <jeizon.borba@gmail.com>
2021-12-13 21:30:11 -0300

    class five - kubernetes

--- golang/cmd/main.go
+++ golang/cmd/main.go
@@ -4,31 +4,40 @@ import (
 	"database/sql"
 	"encoding/json"
 	"log"
+	"os"
 
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jeisonborba/full-cycle-gateway/adapter/broker/kafka"
 	"github.com/jeisonborba/full-cycle-gateway/adapter/factory"
 	"github.com/jeisonborba/full-cycle-gateway/adapter/presenter/transaction"
 	"github.com/jeisonborba/full-cycle-gateway/usecase/process_transaction"
-	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "test.db")
+	db, err := sql.Open("mysql", os.Getenv("MYSQL_USERNAME")+":"+os.Getenv("MYSQL_PASSWORD")+"@tcp("+os.Getenv("MYSQL_HOST")+":3306)/"+os.Getenv("MYSQL_DATABASE"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	repositoryFactory := factory.NewRepositoryDatabaseFactory(db)
 	repository := repositoryFactory.CreateTransactionRepository()
 	configMapProducer := &ckafka.ConfigMap{
-		"bootstrap.servers": "host.docker.internal:9094",
+		"bootstrap.servers": os.Getenv("BOOTSTRAP_SERVERS"),
+		"security.protocol": os.Getenv("SECURITY_PROTOCOL"),
+		"sasl.mechanisms":   os.Getenv("SASL_MECHANISMS"),
+		"sasl.username":     os.Getenv("SASL_USERNAME"),
+		"sasl.password":     os.Getenv("SASL_PASSWORD"),
 	}
 	kafkaPresenter := transaction.NewTransactionKafkaPresenter()
 	producer := kafka.NewKafkaProducer(configMapProducer, kafkaPresenter)
 
 	var msgChan = make(chan *ckafka.Message)
 	configMapConsumer := &ckafka.ConfigMap{
-		"bootstrap.servers": "host.docker.internal:9094",
+		"bootstrap.servers": os.Getenv("BOOTSTRAP_SERVERS"),
+		"security.protocol": os.Getenv("SECURITY_PROTOCOL"),
+		"sasl.mechanisms":   os.Getenv("SASL_MECHANISMS"),
+		"sasl.username":     os.Getenv("SASL_USERNAME"),
+		"sasl.password":     os.Getenv("SASL_PASSWORD"),
 		"client.id":         "goapp",
 		"group.id":          "goapp",
 	}
